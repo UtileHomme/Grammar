4e6302dc2b1db1b2ce3315d22ab67b2e7bdf6e9f
UtileHomme <jatinsharma@healthheal.in>
2018-02-25 08:58:25 +0530

    Grammar First Concepts added

---
+++ Grammar_Concepts.php
@@ -0,0 +1,22 @@
+<!-- What is a Phrase -->
+- A group of words which make sense but not complete sense
+Eg - "In a corner"
+
+<!-- What is a Clause -->
+- a group of words which forms part of a sentence and contain a "Subject" and a "Predicate"
+Eg - He has a chain "which is made of gold"
+
+<!-- What is an Adverb -->
+- used to add something to the meaning of a verb, adjective or another adverb
+Eg - He worked the sum "quickly"
+    - The flower is "very" beautiful
+    - She pronounced the word "quite" correctly
+
+<!-- What is a Preposition -->
+- a word used with a "NOUN" or a "PRONOUN" to show how the person or thing denoted by the "NOUN"
+or "PRONOUN" stands in relation to something else
+Eg - The girl is fond "of" music
+
+** Note **
+Eg- He cheated the employees "glaringly" but they never found out
+- glaringly = in a highly obvious way
